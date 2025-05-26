781546ecdcd1e65d1f36fe25f2371f5b5effb199
jamesdoherty987 <jamesdoherty987@gmail.com>
2025-05-26 12:36:32 +0100

    added Symmetric Encrpytion AES-GCM

--- include/FileManager.hpp
+++ include/FileManager.hpp
@@ -1,58 +1,22 @@
 #pragma once
-#include <QListWidget>
-#include <memory>
-#include <unordered_map>
-#include "EnhancedUser.hpp"
-#include "FileOperations.hpp"
 
-template<typename T>
-class FileManagerTemplate {
+#include <QObject>
+#include <QListWidget>
+#include <QString>
+#include <memory>
+#include "User.hpp"
+
+class FileManager : public QObject {
+    Q_OBJECT
 private:
-    std::shared_ptr<T> currentUser;
+    std::unique_ptr<User> currentUser;
     QListWidget* fileList;
-    std::unordered_map<QString, std::shared_ptr<T>> userDatabase;
+    QString encryptedFilesDir;
 
 public:
-    explicit FileManagerTemplate(QListWidget* list) : fileList(list) {}
-
-    void setUser(std::shared_ptr<T> user) {
-        currentUser = std::move(user);
-    }
-
-    std::shared_ptr<T> getCurrentUser() const {
-        return currentUser;
-    }
-
-    void refreshFileList() {
-        if (!currentUser) return;
-
-        fileList->clear();
-        fileList->addItem("Owned Files:");
-        for (const QString& file : currentUser->getOwnedFiles()) {
-            fileList->addItem("  " + file);
-        }
-        fileList->addItem("Shared With You:");
-        for (const QString& file : currentUser->getSharedFiles()) {
-            fileList->addItem("  " + file);
-        }
-    }
-
-    bool addUser(const QString& username, const QString& password) {
-        if (userDatabase.find(username) != userDatabase.end()) {
-            return false;
-        }
-        userDatabase[username] = std::make_shared<T>(username);
-        return true;
-    }
-
-    std::shared_ptr<T> authenticateUser(const QString& username, const QString& password) {
-        auto it = userDatabase.find(username);
-        if (it != userDatabase.end()) {
-            return it->second;
-        }
-        return nullptr;
-    }
-};
-
-// Concrete implementation using EnhancedUser
-using FileManager = FileManagerTemplate<EnhancedUser>; 
+    explicit FileManager(QListWidget* list);
+    void setUser(std::unique_ptr<User> user);
+    User* getCurrentUser() const;
+    void refreshFileList();
+    QString getEncryptedFilesDir() const;
+}; 
