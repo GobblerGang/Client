e962965846884e12bbf22ab9194aeafb26f9a1ab
Ruairí Glackin <ruairiglackin@gmail.com>
2025-06-04 03:27:17 +0100

    changes to CMakeLists.txt

---
+++ src/DataManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ruairi on 04/06/2025.
+//
+
+#include "DataManager.h"
---
+++ src/DataManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Ruairi on 04/06/2025.
+//
+
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+
+
+
+class DataManager {
+
+};
+
+
+
+#endif //DATAMANAGER_H
