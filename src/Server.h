5edbff7bce2ea55bddb824b144fd48f2b2c8270d
eddiephelanfortyfive <145557787+eddiephelanfortyfive@users.noreply.github.com>
2025-06-05 07:13:59 +0100

    eddie/repairing-VaultManager-due-to-Encrypt-with-key (#25)
    * Refactor VaultManager and Auth to use nonce by reference in encrypt_with_key
    
    * Implement payload signing and header setting in Server class
    
    * Refactor sign_payload method to be a member of the Server class
    
    * Add RequestHeaders struct and update set_headers method in Server class
    
    ---------
    
    Co-authored-by: rglackin <128641650+rglackin@users.noreply.github.com>

--- src/Server.h
+++ src/Server.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <optional>
 #include <nlohmann/json.hpp>
 #include <curl/curl.h>
-#include "utils/Config.h"
-
+#include "RequestHeaders.h"
 // Forward declaration
 class Ed25519PrivateKey;
 class PAC;
@@ -136,7 +134,7 @@ private:
 
     std::string sign_payload(const std::vector<uint8_t>& payload, const std::string& nonce, const Ed25519PrivateKey& private_key);
 
-    std::vector<std::string> set_headers(const Ed25519PrivateKey& private_key, const std::string& user_uuid, const nlohmann::json& payload);
+    RequestHeaders set_headers(const Ed25519PrivateKey& private_key, const std::string& user_uuid, const nlohmann::json& payload);
 
     /**
      * @brief Performs a request to the specified URL with the given payload.
